095ea629f74fa6f7425c88d18d99ed6da2e341ba
Pratik Bugade <pratikbugade555@gmail.com>
2025-12-16 15:45:53 +0530

    Data validation is done!!!

--- 1_pydantic.py
+++ 1_pydantic.py
@@ -1,13 +1,13 @@
-from pydantic import BaseModel
-from typing import List,Dict,Optional
+from pydantic import BaseModel,EmailStr,AnyUrl,Field
+from typing import List,Dict,Optional,Annotated
 
 class patient(BaseModel):
     
-    name : str
-    age : int
-    weight : float
-    married : bool = False
-    allergies : Optional[List[str]] = None 
+    name : Annotated[str,Field(max_length=50,title = "name of the patient",description="give the name of the patient in less than 50 chars",examples=["Pratik","kartik","om"])]
+    age : Annotated[int,Field(gt = 0,strict=True)]
+    weight : Annotated[float,Field(gt = 0,strict = True)]
+    married : Annotated[bool,Field(default = False , description = "is the patient married or not")]
+    allergies : Optional[List[str]] = Field(default=None, max_length = 5)
     contact_details : Dict[str,str] 
 
 def insert_patient_data(patient : patient):
@@ -24,7 +24,7 @@ def update_patient_data(patient : patient):
     print(patient.contact_details)    
     print("data updated successfully")
 
-patient_info ={"name":"Pratik","age":"20","weight":"60","married":"NO","contact_details":{"email":"pratikbugade555@gmail.com","phone":"9356910548"}}
+patient_info ={"name":"Pratik","age":20,"weight":60,"married":"NO","contact_details":{"email":"pratikbugade555@gmail.com","phone":"9356910548"}}
 
 patient_1 = patient(**patient_info)
 
