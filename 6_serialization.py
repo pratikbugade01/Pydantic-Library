dd56193550b84b826d23036ae3c58c819f047bef
Pratik Bugade <pratikbugade555@gmail.com>
2026-01-07 13:56:48 +0530

    serialization is done!!!

---
+++ 6_serialization.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+
+class Address(BaseModel):
+
+    city: str
+    state: str
+    pin: str
+
+class patient(BaseModel):
+
+    name: str
+    gender: str
+    age: int
+    address : Address
+
+
+address_dict = {'city':'maligre','state':'maharashtra','pin':'416505'}
+
+Address1 = Address(**address_dict)
+
+patient_dect ={'name':'pratik','gender':'male','age':'20','address':Address1}
+
+patient1 = patient(**patient_dect)
+
+print(patient1)
+print(patient1.address.pin)
+
+
+temp = patient1.model_dump(include=['name'])
+
+print(temp)
+print(type(temp))
